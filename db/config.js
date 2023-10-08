0db7aa8bc10fabd6c56cea786b1e78e447b33a81
munirsiddiqui54 <122617589+munirsiddiqui54@users.noreply.github.com>
2023-10-09 00:18:54 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/Ecom");
+mongoose.connect("mongodb+srv://munir:db54xyz@cluster0.qku4s44.mongodb.net/?retryWrites=true&w=majority/Ecom");
